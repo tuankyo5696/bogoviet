ee9fc5db2d86ba5d4774080926fb33a3ac8ef8c6
Tran Anh Tuan <tuan.trananh@pycogroup.com>
2019-08-27 15:01:38 +0700

    update trangchu

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,18 +1,18 @@
-import React from "react";
-import "./_Home.scss";
-import Carousel from './../../components/UI/Slider/Carousel';
-
+import React from "react"
+import "./_Home.scss"
+import Carousel from "./../../components/UI/Slider/Carousel"
+import HomeBody from "./homeBody/homeBody"
 const Home = props => {
   return (
-    <div className = "menu">
-     <div className="container1">
-        <div className="row no-gutters">
+    <div className="menu">
+      <div className="container1">
+        <div className="row no-gutters carouselHome">
           <Carousel />
         </div>
       </div>
+      <HomeBody />
     </div>
   )
 }
- 
 
-export default Home;
+export default Home
--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,13 +1,16 @@
-import React from "react";
-import BurgerLogo from "./../../assets/images/logo.png";
-import BurgerTitle from '../../assets/images/bogoTitle.png'
-import "./_Logo.scss";
+import React from "react"
+import BurgerLogo from "./../../assets/images/logo.png"
+import BurgerTitle from "../../assets/images/bogoTitle.png"
+import { NavLink } from "react-router-dom"
+import "./_Logo.scss"
 
 const Logo = props => (
-  <div className="LogoImg">
-    <img src={BurgerLogo} alt="MyBurger" className="bogoLogo" />
-    <img src={BurgerTitle} alt="bogoTitle" className="bogoTitle"/>
-  </div>
-);
+  <NavLink to="/">
+    <div className="LogoImg">
+      <img src={BurgerLogo} alt="MyBurger" className="bogoLogo" />
+      <img src={BurgerTitle} alt="bogoTitle" className="bogoTitle" />
+    </div>
+  </NavLink>
+)
 
-export default Logo;
+export default Logo
