862cd992c6ad0d59fe838358fe73b1b1e17bbeba
Tran Anh Tuan <tuan.trananh@pycogroup.com>
2019-08-28 15:07:47 +0700

    update footer

--- src/components/Footer/FooterInfo/FooterInfo.js
+++ src/components/Footer/FooterInfo/FooterInfo.js
@@ -5,7 +5,6 @@ import "./FooterInfo.scss"
 import img1 from "../../../assets/images/footer/map-marker-alt-solid.svg"
 import img2 from "../../../assets/images/footer/phone-alt-solid.svg"
 import img3 from "../../../assets/images/footer/envelope-regular.svg"
-import img4 from "../../../assets/images/footer/facebook-f-brands.svg"
 
 const FooterInfo = () => (
   <div className="col-lg-6 col-md-6 col-xs-6 col-sm-6 f-address">
@@ -19,29 +18,26 @@ const FooterInfo = () => (
             <span className="ctn">{dataFooter[0].content}</span>
           </div>
           <div className="item phone boder-b">
-            <img src={img2} alt="icon" className="iconFooter" />
+            <img src={img1} alt="icon" className="iconFooter" />
             <span className="lbl">{dataFooter[1].title}</span>
             <span className="ctn">
               <NavLink to="/">{dataFooter[1].content}</NavLink>
             </span>
           </div>
           <div className="item email boder-b">
-            <img src={img3} alt="icon" className="iconFooter" />
+            <img src={img2} alt="icon" className="iconFooter" />
             <span className="lbl">{dataFooter[2].title} </span>
             <span className="ctn">
               <NavLink to="/">{dataFooter[2].content}</NavLink>
             </span>
           </div>
           <div className="item">
-            <img src={img4} alt="icon" className="iconFooter" />
-            <a
-              href="https://www.facebook.com/101677661191439/"
-              className="fb"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <span className="lbl">{dataFooter[3].title}</span>
-            </a>
+            <img src={img3} alt="icon" className="iconFooter" />
+
+            <span className="lbl">{dataFooter[3].title}</span>
+            <span className="ctn">
+              <NavLink to="/">{dataFooter[3].content}</NavLink>
+            </span>
           </div>
         </div>
       </div>
--- src/data/dataFooter.js
+++ src/data/dataFooter.js
@@ -1,18 +1,18 @@
 export const dataFooter = [
   {
-    title: `Địa chỉ:`,
-    content: `280 Lê Văn Thọ P11 Q.Gò Vấp `
+    title: `Văn phòng làm việc :`,
+    content: `389 Nguyễn Văn Khối P8 Q.Gò Vấp `
   },
   {
-    title: `Điện thoại:`,
+    title: `Trụ sở sản xuất :`,
+    content: `280 Lê Văn Thọ, P.11, Q.Gò Vấp`
+  },
+  {
+    title: `Hotline :`,
     content: `0917327317`
   },
   {
-    title: `Email:`,
+    title: `Email :`,
     content: `bogoviet.vn@gmail.com`
-  },
-  {
-    title: `Facebook`,
-    content: `https://www.facebook.com/101677661191439/`
   }
 ]
