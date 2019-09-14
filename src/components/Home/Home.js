ee73f491eda5097501734caafb45c3dd43cb4f79
hongphuc2197 <hongphuc2197@gmail.com>
2019-09-14 23:40:47 +0700

    push video commit

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,10 +2,12 @@ import React from "react"
 import "./_Home.scss"
 import HomeBody from "./homeBody/homeBody"
 import VideoBackground from '../../components/UI/VideoBackground/VideoBackground'
+import Carousel from '../../components/UI/Slider/Carousel'
 
-const Home = props => {
+const Home = () => {
   return (
     <div className="menu">
+      <Carousel />
       <VideoBackground />
       <HomeBody />
     </div>
--- src/components/UI/VideoBackground/VideoBackground.js
+++ src/components/UI/VideoBackground/VideoBackground.js
@@ -1,13 +1,55 @@
 import React from "react"
+import YouTube from "react-youtube"
 import "./VideoBackground.scss"
-import video from '../../../assets/videos/burger.mp4'
 
-const VideoBackground = () => {
-  return (
-    <video autoPlay loop id="myVideo">
-      <source src={video} type="video/mp4" />
-    </video>
-  )
+const videoIdA = "ssVKRRtQFso"
+
+class VideoBackground extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      videoId: videoIdA,
+      player: null
+    }
+  }
+
+  onReady = event => {
+    this.setState({
+      player: event.target
+    })
+  }
+
+  onPlayVideo = () => {
+    this.state.player.playVideo()
+  }
+
+  onPauseVideo = () => {
+    this.state.player.pauseVideo()
+  }
+
+  render() {
+    const opts = {
+      playerVars: {
+        autoplay: 1
+      }
+    }
+    return (
+      <div className="video-background">
+        <YouTube
+          opts={opts}
+          videoId={this.state.videoId}
+          onReady={this.onReady}
+        />
+        <div className="grBtn">
+          <button className="blue" onClick={this.onPlayVideo}>
+            Play
+          </button>
+          <button className="orange" onClick={this.onPauseVideo}>
+            Pause
+          </button>
+        </div>
+      </div>
+    )
+  }
 }
-
 export default VideoBackground
